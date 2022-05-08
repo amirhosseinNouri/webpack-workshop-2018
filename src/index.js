0a35607d41ab702263bec55e3f9933abc67b1130
Amirhossein Nouri <amir.h.nouri2000@gmail.com>
2022-05-08 10:47:46 +0430

    add url loader to load images

--- src/index.js
+++ src/index.js
@@ -4,6 +4,12 @@ import makeButton from './button';
 import { makeColorStyle } from './button-styles';
 import './footer.css';
 import './button.css';
+import image from './webpack-logo.jpeg';
+import makeImage from './image';
+console.log(image);
+
+const imageElement = makeImage(image);
+document.body.append(imageElement);
 
 const newButton = makeButton('Click me!!');
 newButton.style = makeColorStyle('teal');
--- webpack.config.js
+++ webpack.config.js
@@ -7,6 +7,9 @@ const modeConfig = (env) => require(`./build-utils/webpack.${env}.js`)(env);
 module.exports = (env, argv) => {
   return webpackMerge.merge(
     {
+      module: {
+        rules: [{ test: /\.jpe?g$/, use: ['url-loader'] }],
+      },
       output: {
         filename: 'bundle.js',
       },
