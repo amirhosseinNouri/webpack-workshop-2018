aa175ece95410d732fed1198d384fdd9559d0091
Amirhossein Nouri <amir.h.nouri2000@gmail.com>
2022-05-14 10:53:57 +0430

    add presets

--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,28 @@
 const webpack = require('webpack');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const presetConfig = require('./build-utils/presets/load-presets');
 
 const webpackMerge = require('webpack-merge');
 
 const modeConfig = (env) => require(`./build-utils/webpack.${env}.js`)(env);
 module.exports = (env, argv) => {
+  console.log({env , argv})
   return webpackMerge.merge(
     {
       module: {
-        rules: [{ test: /\.jpe?g$/, use: [ {loader: 'url-loader' , options: {
-          limit: 5000
-        }}] }],
+        rules: [
+          {
+            test: /\.jpe?g$/,
+            use: [
+              {
+                loader: 'url-loader',
+                options: {
+                  limit: 5000,
+                },
+              },
+            ],
+          },
+        ],
       },
       output: {
         filename: 'bundle.js',
@@ -18,5 +30,6 @@ module.exports = (env, argv) => {
       plugins: [new HtmlWebpackPlugin(), new webpack.ProgressPlugin()],
     },
     modeConfig(argv.mode),
+    presetConfig({ mode: argv.mode, presets: argv.presets }),
   );
 };
